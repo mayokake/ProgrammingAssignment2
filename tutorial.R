0a3d69d629e4ea855d1ca218caeec8270781e46c
mayokake <masataka.ikeda@mayokake.com>
2020-07-29 00:03:30 +0900

    add new lines

--- tutorial.R
+++ tutorial.R
@@ -1,3 +1,17 @@
 dataset_url <- "http://s3.amazonaws.com/practice_assignment/diet_data.zip"
 download.file(dataset_url, "diet_data.zip")
 unzip("diet_data.zip", exdir = "diet_data")
+list.files("diet_data")
+andy <- read.csv("diet_data/Andy.csv")
+head(andy)
+length(andy$Day)
+str(andy)
+summary(andy)
+names(andy)
+andy[1, "Weight"]
+andy[30, "Weight"]
+
+andy[which(andy$Day == 30), "Weight"]
+andy[which(andy[,"Day"] == 30), "Weight"]
+
+subset(andy$Weight, andy$Day==30)
